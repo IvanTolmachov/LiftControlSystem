62330fa669d28c4dc7243a6ce2453c018d49f0f5
IvanT <ivan@edgedev.com.au>
2025-06-03 15:48:34 +1000

    clean-up

--- LiftControlSystem/Domain/Entities/LiftAction.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace LiftControlSystem.Domain.Entities
-{
-    public class LiftAction
-    {
-        public int ActionFloor { get; set; }
-        public LiftActionType ActionType { get; set; }
-    }
-}
